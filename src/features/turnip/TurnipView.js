832910b44e6de235b4f2747b4aec08426ec221ba
rochellev <rochellevroberts@utexas.edu>
2020-07-27 16:25:23 -0700

    create src folder, and make turnip view

---
+++ src/features/turnip/TurnipView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+const TurnipView = () => {
+  return(
+  
+      <View>
+      <Text>Hi</Text>
+      <Text>This is Turnip view</Text>
+    </View>
+  )
+};
+
+export default TurnipView;
