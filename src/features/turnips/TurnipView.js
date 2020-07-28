96f06544352f255b404bce9cfdd074a21bd2949d
rochellev <rochellevroberts@utexas.edu>
2020-07-28 14:48:47 -0700

    add style object

--- src/features/turnips/TurnipView.js
+++ src/features/turnips/TurnipView.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { SundayPrice } from './SundayPrice'
 
 export const TurnipView = () => {
   return(
   
-      <View>
+      <View style={styles.container}>
       <Text>Hi</Text>
       <Text>This is Turnip view</Text>
+      <SundayPrice />
     </View>
   )
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 20,
+    margin: 10,
+  }
+
+});
