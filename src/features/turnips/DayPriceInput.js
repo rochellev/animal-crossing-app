be5dc0566c76f244389e747f6fdbf5124cd3d317
rochellev <rochellevroberts@utexas.edu>
2020-07-30 11:43:04 -0700

    add component for inputing prices for non sunday

---
+++ src/features/turnips/DayPriceInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { Colors } from './commonStyles'
+
+export const PriceInputCard = () => {
+  return(
+    <View>
+      
+    </View>
+  )
+}
