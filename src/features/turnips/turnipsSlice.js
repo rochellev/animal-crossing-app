c370d9c4230a6a9a07caae4d26bb3cf12b094cf0
rochellev <rochellevroberts@utexas.edu>
2020-08-04 16:30:31 -0700

    add inital state of turnip prices

--- src/features/turnips/turnipsSlice.js
+++ src/features/turnips/turnipsSlice.js
@@ -1,7 +1,36 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { nanoid } from '@reduxjs/toolkit'
 
-const initialState = []
+const initialState = [
+  {
+    sunday: 90,
+    monday: {
+      morning: 92,
+      afternoon: 90,
+    },
+    tuesday: {
+      morning: 50,
+      afternoon: 55,
+    },
+    wednesday: {
+      morning: 60,
+      afternoon: 65,
+    },
+    thursday: {
+      morning: 80,
+      afternoon: 85,
+    },
+    friday: {
+      morning: 90,
+      afternoon: 95,
+    },
+    saturday: {
+      morning: 100,
+      afternoon: 105,
+    }
+    
+  }
+]
 
 const turnipsSlice = createSlice({
   name: 'posts',
