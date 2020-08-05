b45272e0984d07b4501932add8ea7c72756168b9
rochellev <rochellevroberts@utexas.edu>
2020-08-05 11:14:53 -0700

    add code to get turnip prices from redux store

--- src/features/turnips/TurnipView.js
+++ src/features/turnips/TurnipView.js
@@ -1,15 +1,19 @@
 import React from "react";
+import { useSelector } from 'react-redux';
 import { View, Text, Image, StyleSheet } from "react-native";
 import { Colors } from "./commonStyles";
 import { SundayPrice } from "./SundayPrice";
 import { PriceInputCard } from "./PriceInputCard";
 
 export const TurnipView = () => {
+  const turnips = useSelector(state => state.turnips)
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Image source={require("../../../images/Turnips_Icon.png")} />
         <Text>Turnip Stonk</Text>
+        <Text>The Sunday init price is {turnips.sunday}</Text>
       </View>
       <View style={styles.inputSection}>
         <SundayPrice />
