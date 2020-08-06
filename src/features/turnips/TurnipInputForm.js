9b81a4cd4df431cef73ceb06ab3c51c19cb312e3
rochellev <rochellevroberts@utexas.edu>
2020-08-06 16:12:16 -0700

    add function to make an array of components to render

--- src/features/turnips/TurnipInputForm.js
+++ src/features/turnips/TurnipInputForm.js
@@ -5,14 +5,30 @@ import { Colors } from "./commonStyles";
 
 import { SundayPrice } from "./SundayPrice";
 import { PriceInputCard } from "./PriceInputCard";
+import { ScrollView } from "react-native-gesture-handler";
 
 export const TurnipInputForm = () => {
   const turnips = useSelector(state => state.turnips);
 
+  function populateCards() {
+    const days = Object.keys(turnips);
+    const cards = [];
+    cards.push(<SundayPrice key={"sunday"} price={turnips.sunday} />);
+    days.pop();
+    for (const day in days) {
+      cards.push(<PriceInputCard key={day} day={day} />);
+    }
+    return cards;
+  }
+
+  const inputCardList = populateCards();
+
   return (
-    <View>
-      <Text>This is TurnipInputForm</Text>
-      <SundayPrice price={turnips.sunday} />
-    </View>
+    <ScrollView>
+      <View style={{ flex: 1, backgroundColor: Colors.blanchedAlmond }}>
+        <Text>This is TurnipInputForm</Text>
+        {inputCardList}
+      </View>
+    </ScrollView>
   );
 };
