35bf1eba35834b8b7c73cc765449827126b5c406
rochellev <rochellevroberts@utexas.edu>
2020-08-06 16:13:10 -0700

    use prop for price

--- src/features/turnips/SundayPrice.js
+++ src/features/turnips/SundayPrice.js
@@ -1,33 +1,33 @@
-import React, {useState} from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
-import { Colors } from './commonStyles';
-import Slider from '@react-native-community/slider';
+import React, { useState } from "react";
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import { Colors } from "./commonStyles";
+import Slider from "@react-native-community/slider";
 
-export const SundayPrice = () => {
-  const [price, setPrice] = useState('0');
-  var initNumber = parseInt(price);
-  return(
-      <View style={styles.container}>
-      <Text>Sunday</Text><Text>{price}</Text>
+// onValueChange={value => setPrice(value)}
+
+export const SundayPrice = ({ price }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Sunday</Text>
+      <Text>{price}</Text>
       <Slider
         style={styles.slider}
+        value={price}
         minimumValue={1}
         maximumValue={400}
         step={1}
-        onValueChange={value => setPrice(value)}
-      
       />
     </View>
-  )
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
     backgroundColor: Colors.oldLace
   },
   slider: {
-    width: 150,
+    width: 150
   }
 });
