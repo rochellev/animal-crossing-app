f0a5a4e68d918618760693234c905e898d5fe5d4
rochellev <rochellevroberts@utexas.edu>
2020-08-06 12:14:02 -0700

    fix init state, did not want an array of turnip price objects, rather just one week worths of prices

--- src/features/turnips/turnipsSlice.js
+++ src/features/turnips/turnipsSlice.js
@@ -1,35 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [
-  {
-    sunday: 90,
-    monday: {
-      morning: 92,
-      afternoon: 90
-    },
-    tuesday: {
-      morning: 50,
-      afternoon: 55
-    },
-    wednesday: {
-      morning: 60,
-      afternoon: 65
-    },
-    thursday: {
-      morning: 80,
-      afternoon: 85
-    },
-    friday: {
-      morning: 90,
-      afternoon: 95
-    },
-    saturday: {
-      morning: 100,
-      afternoon: 105
-    }
+const initialState = {
+  sunday: 90,
+  monday: {
+    morning: 92,
+    afternoon: 90
+  },
+  tuesday: {
+    morning: 50,
+    afternoon: 55
+  },
+  wednesday: {
+    morning: 60,
+    afternoon: 65
+  },
+  thursday: {
+    morning: 80,
+    afternoon: 85
+  },
+  friday: {
+    morning: 90,
+    afternoon: 95
+  },
+  saturday: {
+    morning: 100,
+    afternoon: 105
   }
-];
-
+};
 const turnipsSlice = createSlice({
   name: "turnips",
   initialState,
