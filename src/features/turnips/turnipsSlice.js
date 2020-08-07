6552ca251ebd041f9d0e315ac2e8499640536d23
rochellev <rochellevroberts@utexas.edu>
2020-08-07 13:54:15 -0700

    add day field to initalState

--- src/features/turnips/turnipsSlice.js
+++ src/features/turnips/turnipsSlice.js
@@ -3,26 +3,32 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   sunday: 90,
   monday: {
+    day: "Monday",
     morning: 92,
     afternoon: 90
   },
   tuesday: {
+    day: "Tuesday",
     morning: 50,
     afternoon: 55
   },
   wednesday: {
+    day: "Wednesday",
     morning: 60,
     afternoon: 65
   },
   thursday: {
+    day: "Thursday",
     morning: 80,
     afternoon: 85
   },
   friday: {
+    day: "Friday",
     morning: 90,
     afternoon: 95
   },
   saturday: {
+    day: "Saturday",
     morning: 100,
     afternoon: 105
   }
