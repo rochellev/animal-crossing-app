52b19f40821e416cbf72594c8f8afee4af871d76
rochellev <rochellevroberts@utexas.edu>
2020-08-07 11:29:18 -0700

    add second slider for pm, displaying numbers from state

--- src/features/turnips/PriceInputCard.js
+++ src/features/turnips/PriceInputCard.js
@@ -3,19 +3,34 @@ import { View, Text, Image, StyleSheet } from "react-native";
 import { Colors } from "./commonStyles";
 import Slider from "@react-native-community/slider";
 
-export const PriceInputCard = ({ day, morning }) => {
+export const PriceInputCard = ({ day, morning, afternoon }) => {
   return (
     <View style={styles.container}>
-      <Text>
-        {day}...............{morning}
-      </Text>
-      <Slider
-        style={{ width: 200, height: 40, backgroundColor: Colors.lavenderGrey }}
-        minimumValue={0}
-        maximumValue={1}
-        minimumTrackTintColor="#FFFFFF"
-        maximumTrackTintColor="#000000"
-      />
+      <Text>{day}</Text>
+      <View style={styles.cards}>
+        <View style={styles.cardInput}>
+          <Text>AM.....{morning}</Text>
+          <Slider
+            style={styles.slider}
+            value={morning}
+            minimumValue={0}
+            maximumValue={850}
+            minimumTrackTintColor="#FFFFFF"
+            maximumTrackTintColor="#000000"
+          />
+        </View>
+        <View style={styles.cardInput}>
+          <Text>PM.....{afternoon}</Text>
+          <Slider
+            style={styles.slider}
+            value={afternoon}
+            minimumValue={0}
+            maximumValue={850}
+            minimumTrackTintColor="#FFFFFF"
+            maximumTrackTintColor="#000000"
+          />
+        </View>
+      </View>
     </View>
   );
 };
@@ -24,5 +39,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center"
+  },
+  cards: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  cardInput: {
+    padding: 5
+  },
+  slider: {
+    width: 150,
+    height: 30,
+    backgroundColor: Colors.lavenderGrey
   }
 });
