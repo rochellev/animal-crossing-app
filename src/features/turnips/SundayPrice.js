1a7fb710282624efcbb21da3a6fc442031ccff25
rochellev <rochellevroberts@utexas.edu>
2020-08-12 14:35:52 -0700

    add state const for input

--- src/features/turnips/SundayPrice.js
+++ src/features/turnips/SundayPrice.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 import { View, Text, TextInput, StyleSheet } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
 import { Colors } from "./commonStyles";
 import { TurnipSlider } from "./TurnipSlider";
 
 // onValueChange={value => setPrice(value)}
 
 export const SundayPrice = ({ ...props }) => {
+  const [input, setInput] = useState("");
+
+  const dispatch = useDispatch();
+
   return (
     <View style={styles.container}>
       <Text>Sunday</Text>
