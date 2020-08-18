81fc57f70958db602dc3e54809b78b673ad56bc7
rochellev <rochellevroberts@utexas.edu>
2020-08-18 16:24:30 -0700

    add loading to state

--- src/features/turnips/turnipsSlice.js
+++ src/features/turnips/turnipsSlice.js
@@ -1,38 +1,41 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import AsyncStorage from "@react-native-community/async-storage";
 
 const initialState = {
-  sunday: 90,
-  monday: {
-    day: "Monday",
-    morning: 40,
-    afternoon: 400
+  turnips: {
+    sunday: 90,
+    monday: {
+      day: "Monday",
+      morning: 40,
+      afternoon: 400
+    },
+    tuesday: {
+      day: "Tuesday",
+      morning: 50,
+      afternoon: 500
+    },
+    wednesday: {
+      day: "Wednesday",
+      morning: 60,
+      afternoon: 650
+    },
+    thursday: {
+      day: "Thursday",
+      morning: 80,
+      afternoon: 80
+    },
+    friday: {
+      day: "Friday",
+      morning: 90,
+      afternoon: 200
+    },
+    saturday: {
+      day: "Saturday",
+      morning: 100,
+      afternoon: 800
+    }
   },
-  tuesday: {
-    day: "Tuesday",
-    morning: 50,
-    afternoon: 500
-  },
-  wednesday: {
-    day: "Wednesday",
-    morning: 60,
-    afternoon: 650
-  },
-  thursday: {
-    day: "Thursday",
-    morning: 80,
-    afternoon: 80
-  },
-  friday: {
-    day: "Friday",
-    morning: 90,
-    afternoon: 200
-  },
-  saturday: {
-    day: "Saturday",
-    morning: 100,
-    afternoon: 800
-  }
+  status: "idle",
+  error: null
 };
 const turnipsSlice = createSlice({
   name: "turnips",
