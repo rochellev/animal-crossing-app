0e4d5766e1b87a0226bba0a28c747f099a7e8cbe
rochellev <rochellevroberts@utexas.edu>
2020-09-22 16:58:56 -0700

    add method to get data, plan on using async later so i have status and error as part of redux state

--- src/features/predictions/predictionsSlice.js
+++ src/features/predictions/predictionsSlice.js
@@ -3,22 +3,52 @@ import AsyncStorage from "@react-native-community/async-storage";
 import { useAsyncStorage } from "@react-native-community/async-storage";
 const { getItem, setItem } = useAsyncStorage("@data");
 
+// need to know turnips array for input to predictions
+
 const initialState = {
   data: {
     firstTimeBuyer: false,
-    previousPattern: 'LARGE_SPIKE',
+    previousPattern: "LARGE_SPIKE",
+    output: {
+      monday: {
+        guaranteedMin: 20,
+        potentialMax: 899
+      },
+      tuesday: {
+        guaranteedMin: 20,
+        potentialMax: 899
+      },
+      wednesday: {
+        guaranteedMin: 20,
+        potentialMax: 899
+      },
+      thursday: {
+        guaranteedMin: 20,
+        potentialMax: 899
+      },
+      friday: {
+        guaranteedMin: 20,
+        potentialMax: 899
+      },
+      saturday: {
+        monday: {
+          guaranteedMin: 20,
+          potentialMax: 899
+        },
+      }
+    }
+  },
+  status: "idle",
+  error: null,
 
-  }
-
-}
+};
 
 const predictionsSlice = createSlice({
   name: "predictions",
   initialState,
-  reducers: {
-    
-  }
+  reducers: {}
 });
 
 export default predictionsSlice.reducer;
 
+export const getData = (state) => Object.values(state.predictions.data)
