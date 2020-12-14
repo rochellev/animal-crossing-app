1057a7124798a9d21578ab47fcac4e4bd241dcac
rochellev <rochellevroberts@utexas.edu>
2020-12-14 11:21:50 -0800

    define config and data objects for the line chart

--- src/features/predictions/PredictionChart.js
+++ src/features/predictions/PredictionChart.js
@@ -6,44 +6,68 @@ import { Colors, AppStyles } from "../styles/AppStyles";
 import { LineChart } from "react-native-chart-kit";
 import { Dimensions } from "react-native";
 
-const labels = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
-const data = [
-  Math.random() * 100,
-  Math.random() * 100,
-  Math.random() * 100,
-  Math.random() * 100,
-  Math.random() * 100,
-  Math.random() * 100,
-  Math.random() * 100
-];
+
+const data = {
+  labels: [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ],
+  datasets: [
+    {
+      data: [
+        Math.random() * 100,
+        Math.random() * 100,
+        Math.random() * 100,
+        Math.random() * 100,
+        Math.random() * 100,
+        Math.random() * 100,
+        Math.random() * 100
+      ],
+      color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+      strokeWidth: 2 // optional
+    }
+  ],
+};
+
+const chartConfig = {
+  backgroundColor: "#e26a00",
+  backgroundGradientFrom: "#D9D9D9",
+  backgroundGradientTo: "#FFF",
+  decimalPlaces: 0, // optional, defaults to 2dp
+  color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+  labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+  style: { borderRadius: 16, paddingVertical: 5 },
+  propsForDots: { r: "6", strokeWidth: "2", stroke: "#ffa726" }
+};
 
 export const PredictionChart = () => {
   return (
     <LineChart
-      data={{ labels: labels, datasets: [{ data: data }] }}
+      data={data}
       width={Dimensions.get("window").width - 15}
       height={300}
-      chartConfig={{
-        backgroundColor: "#e26a00",
-        backgroundGradientFrom: "#D9D9D9",
-        backgroundGradientTo: "#FFF",
-        decimalPlaces: 0, // optional, defaults to 2dp
-        color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-        labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-        style: { borderRadius: 16 },
-        propsForDots: { r: "6", strokeWidth: "2", stroke: "#ffa726" }
-      }}
-      bezier
+      chartConfig={chartConfig}
       style={{ marginVertical: 15, borderRadius: 15, paddingTop: 15 }}
       verticalLabelRotation={25}
+      bezier
     />
   );
 };
+
+// chartConfig={{
+//   backgroundColor: "#e26a00",
+//   backgroundGradientFrom: "#D9D9D9",
+//   backgroundGradientTo: "#FFF",
+//   decimalPlaces: 0, // optional, defaults to 2dp
+//   color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+//   labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+//   style: { borderRadius: 16 },
+//   propsForDots: { r: "6", strokeWidth: "2", stroke: "#ffa726" }
+// }}
+
+// data={{ labels: labels, datasets: [{ data: data }] }}
